e8f21897861b3952675fb873e31c67ab4cb4a898
ror.shaan <ror.shaan@gmail.com>
2024-02-29 18:37:27 +0530

    add spec for single string digit

--- calculator_spec.rb
+++ calculator_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe Calculator do
       it 'returns 0 for an empty string' do
         expect(Calculator.add([""])).to eq([0])
       end
+
+      it 'returns the number itself for a single number' do
+        expect(Calculator.add(["1"])).to eq([1])
+      end
     end
   end
 end
